75c02409542809cbc5ead3631f13a52a70a2e6a1
Isaac <isaacxpreston@gmail.com>
2018-02-07 11:54:55 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="container">
+    <div class="dragMe"></div>
+    <!-- <div class="dragMe"></div> -->
+  </div>
+</template>
+
+<script>
+  import interact from 'interactjs'
+
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        originalX: null
+      }
+    },
+    mounted() {
+      
+      // resize / reposition elements on window resize
+      window.addEventListener('resize', () => {
+
+        let elOffsetX = parseInt(document.querySelector('.dragMe').getAttribute('data-x')) || 0
+        let elOffsetY = parseInt(document.querySelector('.dragMe').getAttribute('data-y')) || 0
+        let elWidth = parseInt(window.getComputedStyle(document.querySelector('.dragMe'))['width'].replace('px', ''))
+        let elCombinedX = elOffsetX + elWidth
+
+        let containerWidth = parseInt(window.getComputedStyle(document.querySelector('.container'))['width'].replace('px', ''))
+
+
+        if (elCombinedX > containerWidth) {
+            this.originalX = elOffsetX
+            let newOffsetX = (containerWidth - elWidth) // - 24 // ?? not sure why I need 10px here.
+            document.querySelector('.dragMe').style.transform = 'translate(' + newOffsetX + 'px, ' + elOffsetY + 'px)'
+            document.querySelector('.dragMe').setAttribute('data-x', newOffsetX)
+        }
+
+      })
+
+
+
+      function dragMoveListener(event) {
+        var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+      }
+
+      interact('.dragMe')
+        .draggable({
+          onmove: dragMoveListener,
+          restrict: {
+            restriction: 'parent',
+            elementRect: {
+              top: 0,
+              left: 0,
+              bottom: 1,
+              right: 1
+            }
+          }
+        })
+
+        .resizable({
+
+          // resize from all edges and corners  
+          edges: {
+            left: true,
+            right: true,
+            bottom: true,
+            top: true
+          },
+
+          // keep the edges inside the parent
+          restrictEdges: {
+            outer: 'parent',
+            endOnly: true,
+          },
+
+          // minimum size
+          restrictSize: {
+            min: {
+              width: 100,
+              height: 50
+            },
+          },
+
+          inertia: false
+        })
+        .on('resizemove', function (event) {
+          console.log('resizemove event fired')
+          var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+          // update the element's style
+          target.style.width = event.rect.width + 'px';
+          target.style.height = event.rect.height + 'px';
+
+          // translate when resizing from top or left edges
+          x += event.deltaRect.left;
+          y += event.deltaRect.top;
+
+          target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+          target.setAttribute('data-x', x);
+          target.setAttribute('data-y', y);
+          // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+        })
+
+    }
+  }
+
+</script>
+
+<style scoped lang='scss'>
+  .container {
+    position: absolute;
+    box-sizing: border-box;
+    border: 5px solid rgba(255, 0, 0, 0.5);
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    width: 1200px;
+    height: 600px;
+    max-width: 100vw;
+    max-height: 100vh;
+    background: rgba(255, 0, 0, 0.15);
+  }
+
+  .dragMe {
+    position: relative;
+    width: 200px;
+    height: 200px;
+    background: blue; // cursor: pointer;
+    box-sizing: border-box;
+    border: 12px solid rgba(255, 0, 0, 0.5);
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+</style>
